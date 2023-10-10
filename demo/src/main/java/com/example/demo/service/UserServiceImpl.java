33710ef764eebc6d712b1d74fd8f9f7ce9923421
Vera2805 <vera22rus@gmail.com>
2023-10-10 22:51:59 +0700

    Initial commit

--- demo/src/main/java/com/example/demo/service/UserServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -3,14 +3,13 @@ package com.example.demo.service;
 import com.example.demo.dao.UserDao;
 import com.example.demo.model.User;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
+
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@Transactional(propagation = Propagation.NEVER)
+@Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
     private final UserDao userDao;
 
@@ -18,31 +17,31 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
-    @Transactional(readOnly = true)
+
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
-    @Transactional(propagation = Propagation.NEVER)
+
     @Override
     public User getUser(Long id) {
         return userDao.getUser(id);
 
     }
 
-    @Transactional(isolation = Isolation.SERIALIZABLE)
+    @Transactional
     @Override
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
-    @Transactional(timeout = 10)
+    @Transactional
     @Override
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
-
+    @Transactional
     public void deleteUser(Long id) {
         userDao.deleteUser(id);
 
